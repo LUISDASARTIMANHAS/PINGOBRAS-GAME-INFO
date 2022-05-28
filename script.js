08f5ce7564cef28ae2bc619dc36d8648e8a5d5a6
Glitch (pingobras-apresentacao) <none>
2022-05-28 19:22:44 +0000

    🚠🐊 Checkpoint ./style.css:26026384/368 ./pesquisar.html:26026384/6 ./script.js:26026384/642

--- script.js
+++ script.js
@@ -41,3 +41,22 @@ fsEnter.addEventListener('click', function (e) {
         fsEnter.innerHTML = "TELA CHEIA";
     }
 });
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("pesquisador");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "hidden";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
