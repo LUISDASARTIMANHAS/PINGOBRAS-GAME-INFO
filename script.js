33e3bb1805dd527157a8b932e9b9606fd39aaddd
Glitch (pingobras-apresentacao) <none>
2022-05-28 19:34:29 +0000

    💽📇 Checkpoint ./style.css:26026384/63 ./script.js:26026384/64 ./pesquisar.html:26026384/16

--- script.js
+++ script.js
@@ -47,14 +47,14 @@ function myFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("pesquisador");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
+  ul = document.getElementById("menuSearch");
   li = ul.getElementsByTagName("li");
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "hidden";
+      li[i].style.display = "";
     } else {
       li[i].style.display = "none";
     }
